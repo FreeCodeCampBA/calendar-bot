72aedea987b3fd6c11de926f3e4bd99b9e3fcb60
Hugo Farji <hdf1986.6700@gmail.com>
2019-03-02 20:36:04 -0300

    Added dotenv & random avatar

--- config.js
+++ config.js
@@ -1,4 +1,19 @@
+require('dotenv').config()
+
 module.exports = {
-  CHANNEL: 'eventos-prueba',
-  API_URL: 'https://calendar-api.now.sh/'
+  CHANNEL: process.env.CHANNEL,
+  API_URL: process.env.API_URL || 'https://calendar-api.now.sh/',
+  HOOK_URL: process.env.HOOK_URL,
+  BOTS: [{
+     "name": "Agustin Mulet",
+     "avatar": "https://calendar-bot.now.sh/assets/agustin.jpeg"
+   },
+   {
+     "name": "Goncy",
+     "avatar": "https://calendar-bot.now.sh/assets/gonzo.jpeg"
+   },
+   {
+     "name": "Nay",
+     "avatar": "https://calendar-bot.now.sh/assets/nay.jpeg"
+   }]
 }
--- src/bot.js
+++ src/bot.js
@@ -1,30 +1,25 @@
+const Slack = require('slack-node')
 const moment = require('moment-timezone');
 
-const { CHANNEL } = require('../config');
+const { CHANNEL, BOTS, HOOK_URL } = require('../config');
 const messageBuilder = require('./messageBuilder');
 const getEvents = require('./getEvents');
 
 const TIMEZONE = 'America/Buenos_Aires'
 const timeFormat = 'HH:mm:ss';
 
-const Slack = require('slack-node')
-
 moment.locale('es')
 
+const getBot = () => BOTS[Math.floor(Math.random() * BOTS.length)];
+
 const sendMessage = (options) => {
-  const messageOptions = {
-      channel: process.env.CHANNEL,
-      icon_emoji: '',
-      text: 'test',
-      username: 'randomBot.name'
-  }
   const slack = new Slack()
 
-  slack.setWebhook('https://hooks.slack.com/services/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')
+  slack.setWebhook(HOOK_URL)
   slack.webhook(options, (error, response) => {
-      if (error) return console.error(error)
+    if (error) return console.error(error)
 
-      return console.log('Mensaje en Slack exitoso!')
+    return console.log('Mensaje en Slack exitoso!')
   })
 }
 
@@ -33,11 +28,14 @@ const run = async () => {
   const morning = time.isBetween(moment('00:00:00', timeFormat), moment('12:00:00', timeFormat))
   const dayToRetrieve = morning ? time : time.add(4, 'day')
   const events = (await getEvents(dayToRetrieve));
+  const profile = getBot();
 
   if(events.length === 0) return console.log(`No hay eventos para el ${moment.tz(new Date(), TIMEZONE).format('YYYY-MM-DD')}`)
 
   sendMessage({
     channel: CHANNEL,
+    icon_emoji: profile.avatar,
+    username: profile.name,
     text: messageBuilder(morning, events)
   })
 }
