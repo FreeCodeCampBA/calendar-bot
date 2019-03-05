e08df67081a0dc5f14a77f1737502fe29b2073c4
Hugo Farji <hdf1986.6700@gmail.com>
2019-03-05 17:39:19 -0300

    Unhardcoded 4 days

--- src/bot.js
+++ src/bot.js
@@ -26,7 +26,7 @@ const sendMessage = (options) => {
 const run = async () => {
   const time = moment(new Date(), TIMEZONE);
   const morning = time.isBetween(moment('00:00:00', timeFormat), moment('12:00:00', timeFormat))
-  const dayToRetrieve = morning ? time : time.add(4, 'day')
+  const dayToRetrieve = morning ? time : time.add(1, 'day')
   const events = (await getEvents(dayToRetrieve));
   const profile = getBot();
 
